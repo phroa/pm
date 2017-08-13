6e4091670425381da37f5048950980e4a2bda9bd
Jack Stratton <jack@phroa.net>
2017-08-13 14:42:35 -0700

    Shadow Gson because SpongeVanilla doesn't

--- src/main/java/net/phroa/pm/Pm.java
+++ src/main/java/net/phroa/pm/Pm.java
@@ -67,7 +67,7 @@ public class Pm {
                 .connectionSpecs(ImmutableList.of(ConnectionSpec.MODERN_TLS))
                 .addInterceptor(chain -> chain.proceed(chain.request().newBuilder()
                         .header("X-Adoreable", "meow")
-                        .header("User-Agent", "pm/0.2.0-SNAPSHOT " + chain.request().header("User-Agent"))
+                        .header("User-Agent", "pm/0.2.1-SNAPSHOT " + chain.request().header("User-Agent"))
                         .build()))
                 .addNetworkInterceptor(chain -> {
                     if (!chain.request().isHttps()) {
